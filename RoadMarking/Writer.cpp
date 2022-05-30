da0bbc887269575683f7ace79f7f031bb8fce757
Elizaveta Prishlyak <104158586+Ezizaveta@users.noreply.github.com>
2022-05-30 11:01:13 +0700

    Road Marking
    Program simulates operation of road marking machine. The input file includes the lenghts and types of lines which follow each other. An output file includes coords and commands "up" or "down" for paint roller.

---
+++ RoadMarking/Writer.cpp
@@ -0,0 +1,18 @@
+#include "Writer.h"
+
+namespace
+{
+	std::string toString(CommandType type)
+	{
+		return type == up ? "up" : "down";
+	}
+}
+void Writer::writeRoadMarking(const std::vector<Command>& commands)
+{
+	if (commands.empty()) return;
+	for (size_t i = 0; i < commands.size(); i++)
+	{
+		os << commands[i].coord << " ";
+		os << toString(commands[i].type) << '\n';
+	}
+}
---
+++ RoadMarking/Writer.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+#include "Line.h"
+
+class Writer
+{
+private:
+	std::ostream& os;
+
+public:
+	Writer(std::ostream& _os) : os(_os) {}
+	void writeRoadMarking(const std::vector<Command>& commands);
+};
